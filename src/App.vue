d2639293b53916d57e0e0144fe6125303fc2bb09
misov <miso.valent@gmail.com>
2020-03-06 23:59:49 +0100

    CH 01 finished

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,26 @@
     </h4>
 
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
-      </div>
 
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="t.done" class="form-check-input" />
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nothing to do. Hurrah!</b>
         </div>
       </div>
+
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input" />
+          </div>
+        </div>
+      </template>
 
       <div class="row py-2">
           <div class="col">
@@ -34,6 +43,13 @@
                   Hide completed tasks
               </label>
           </div>
+
+          <div class="col text-center">
+            <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+              Delete Completed
+            </button>
+          </div>
+
       </div>
     </div>
 
@@ -68,9 +84,19 @@ export default {
   methods: {
     addNewTodo() {
       this.tasks.push({action: this.newItemText, done: false});
-      localStorage.setItem("todos", JSON.stringify(this.tasks));
+      //localStorage.setItem("todos", JSON.stringify(this.tasks));
+      this.storeData();
       this.newItemText = "";
-    }    
+    },
+
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
+    }
   },
 
   created() {
